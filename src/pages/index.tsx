72343373644f6d526dbbf3f2f627aac8263dba70
Dervex <dervexdev@gmail.com>
2024-03-31 00:05:20 +0100

    Remove client-side code from server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import Layout from "@theme/Layout";
 import Features from "@site/src/components/Features";
 import Heading from "@theme/Heading";
@@ -13,32 +14,39 @@ import Banner from "@site/static/img/banner.svg";
 import styles from "./index.module.css";
 
 function Header() {
-  const { width } = useWindowDimensions();
   const { siteConfig } = useDocusaurusContext();
-
-  let player: ReactPlayer;
-
-  function onProgress(state: OnProgressProps) {
-    if (state.playedSeconds >= 73) {
-      player.seekTo(0);
-    }
-  }
 
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className={styles.dimmer} />
 
-      <ReactPlayer
-        className={styles.player}
-        ref={(ref) => (player = ref)}
-        url={"https://www.youtube.com/watch?v=eAvXhNlO-rA"}
-        playing={true}
-        muted={true}
-        loop={true}
-        width={width}
-        height={width * 2}
-        onProgress={onProgress}
-      />
+      <BrowserOnly>
+        {() => {
+          const { width } = useWindowDimensions();
+
+          let player: ReactPlayer;
+
+          function onProgress(state: OnProgressProps) {
+            if (state.playedSeconds >= 73) {
+              player.seekTo(0);
+            }
+          }
+
+          return (
+            <ReactPlayer
+              className={styles.player}
+              ref={(ref) => (player = ref)}
+              url={"https://www.youtube.com/watch?v=eAvXhNlO-rA"}
+              playing={true}
+              muted={true}
+              loop={true}
+              width={width}
+              height={width * 2}
+              onProgress={onProgress}
+            />
+          );
+        }}
+      </BrowserOnly>
 
       <div className={clsx("container", styles.container)}>
         <Heading as="h1" className="hero__title">
